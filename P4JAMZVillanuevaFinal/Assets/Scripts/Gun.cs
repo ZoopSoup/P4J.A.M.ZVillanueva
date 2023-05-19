cbd4d5d2ec6eb7f433690a28ffcb8e337606362b
ZoopSoup <zoopidysoupidy@gmail.com>
2023-05-19 12:33:47 -0700

    Finished Player Movement
    Finished Map + Player Movement

---
+++ P4JAMZVillanuevaFinal/Assets/Scripts/Gun.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public UnityEvent OnGunShoot;
+    public float FireCooldown;
+
+
+    public bool Automatic;
+
+    private float CurrentCoolDown;
+    // Start is called before the first frame update
+    void Start()
+    {
+        CurrentCoolDown = FireCooldown;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Automatic)
+        {
+            if (Input.GetMouseButton(0))
+            {
+                if (CurrentCoolDown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCoolDown = FireCooldown;
+                }
+            }
+        }
+        else
+        {
+            if (Input.GetMouseButtonDown(0))
+            {
+                if (CurrentCoolDown <= 0f)
+                {
+                    OnGunShoot?.Invoke();
+                    CurrentCoolDown = FireCooldown;
+                }
+            }
+        }
+
+        CurrentCoolDown -= Time.deltaTime;
+    }
+}
