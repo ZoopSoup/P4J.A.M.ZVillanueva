57b7ee0fd593ba9cb73e6145eef64975f8add69d
ZoopSoup <zoopidysoupidy@gmail.com>
2023-05-22 20:32:20 -0700

    added gun
    added gun using raycasting
    playable demo

--- P4JAMZVillanuevaFinal/Assets/Scripts/Gun.cs
+++ P4JAMZVillanuevaFinal/Assets/Scripts/Gun.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.Events;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
---
+++ P4JAMZVillanuevaFinal/Assets/Scripts/GunDamage.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunDamage : MonoBehaviour
+{
+    public float Damage;
+    public float BulletRange;
+    private Transform PlayerCamera;
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        PlayerCamera = Camera.main.transform;
+    }
+
+    // Update is called once per frame
+    public void Shoot()
+    {
+        Ray gunRay = new Ray(PlayerCamera.position, PlayerCamera.forward);
+        if (Physics.Raycast(gunRay, out RaycastHit hitInfo, BulletRange))
+        {
+            if (hitInfo.collider.gameObject.TryGetComponent(out Zombie enemy))
+            {
+                enemy.Health -= Damage;
+            }
+        }
+    }
+}
